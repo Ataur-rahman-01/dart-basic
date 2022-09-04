b96267dbf2c8dd8ad127e31cd9b1320147b2a60a
Ataur <iamataur0911@gmail.com>
2022-09-04 17:48:57 +0600

    first commit

---
+++ variable.dart
@@ -0,0 +1,16 @@
+main() {
+  String fristName = "Ataur";
+  String lastName = "Rahman";
+  int age = 24;
+  double gpa = 3.5;
+  bool isPass = true;
+  var fatherName = "Golam rahman";
+  var fatherAge = 60;
+  dynamic ocupation = "job";
+  const motherName = "anju";
+  print("My name is ${fristName + " " + lastName}");
+  print("My age is ${age}");
+  ocupation = "doc";
+  print("${fatherName} job is ${ocupation} age is ${fatherAge}");
+  print("My gpa is ${gpa} ${isPass} my mother name is ${motherName}");
+}
