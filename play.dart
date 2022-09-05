f47c52ea7848636995f8f073edf555a8d95203b7
Ataur <iamataur0911@gmail.com>
2022-09-05 18:17:09 +0600

    adding

--- play.dart
+++ play.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+import 'dart:math';
+
 main() {
-  print("hello world");
+  print("Enter a number?");
+  String num1 = stdin.readLineSync()!;
+  print("Enter 2nd number?");
+  String num2 = stdin.readLineSync()!;
+  print("Answer:${double.parse(num1) + double.parse(num2)}");
 }
